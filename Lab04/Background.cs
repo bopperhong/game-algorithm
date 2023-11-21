98547b5f973017928556f4dcb054daedf0f4239f
bopperhong <79531280+bopperhong@users.noreply.github.com>
2023-11-21 16:03:34 +0800

    Add files via upload
    Lab04

---
+++ Lab04/Background.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using GameAlgoT2310;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Linq;
+
+namespace Lab04
+{
+	public class Background : GameObject
+	{
+
+		public Texture2D Texture;
+		public Rectangle Rectangle;
+
+		public Background(string name) : base(name)
+		{
+
+		}
+
+        public override void Initialize()
+        {
+			Texture = _game.Content.Load<Texture2D>("purple");
+			Rectangle.X = 0;
+			Rectangle.Y = 0;
+			Rectangle.Width = _game.Graphics.PreferredBackBufferWidth;
+			Rectangle.Height = _game.Graphics.PreferredBackBufferHeight;
+
+        }
+
+		public override void Draw()
+		{
+			_game.SpriteBatch.Begin(samplerState: SamplerState.LinearWrap);
+			_game.SpriteBatch.Draw(Texture, Vector2.Zero, Rectangle, Color.White);
+			_game.SpriteBatch.End();
+		}
+    }
+}
+
---
+++ Lab04/Spaceship.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using GameAlgoT2310;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Linq;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lab04
+{
+	public class Spaceship : GameObject
+	{
+
+		public Texture2D spaceShip_texture;
+		public Vector2 spaceShipPosition;
+		float flyingSpeed;
+
+		public Spaceship(string name) : base(name)
+		{
+
+		}
+
+        public override void Initialize()
+        {
+			spaceShip_texture = _game.Content.Load<Texture2D>("spaceShips_009_right");
+			spaceShipPosition = new Vector2(_game.Graphics.PreferredBackBufferWidth / 2, _game.Graphics.PreferredBackBufferHeight / 2);
+			flyingSpeed = 100f;
+
+        }
+
+		public override void Draw()
+		{
+			_game.SpriteBatch.Begin();
+			_game.SpriteBatch.Draw(spaceShip_texture, spaceShipPosition, null, Color.White);
+			_game.SpriteBatch.End();
+		}
+
+        public override void Update()
+        {
+
+			var kState = Keyboard.GetState();
+			if (kState.IsKeyDown(Keys.W))
+			{
+				spaceShipPosition.Y -= flyingSpeed * ScalableGameTime.DeltaTime;
+			}
+		}
+	}
+}
+
